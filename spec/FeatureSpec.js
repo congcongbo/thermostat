89a88b5b067a1859ffd275a9d256bdb5e3577fc8
EOSullivanBerlin <esully666@gmail.com>
2017-08-02 16:21:59 +0200

    Changing the Max Temp

--- spec/FeatureSpec.js
+++ spec/FeatureSpec.js
@@ -8,21 +8,42 @@ describe('Thermostat', function() {
   });
 
   it('starts at 20', function() {
-    expect(thermostat.temp).toBe(20);
+    expect(thermostat._temp).toBe(20);
   });
 
   it('can increase the temp', function() {
     thermostat.up(1)
-    expect(thermostat.temp).toBe(21);
+    expect(thermostat._temp).toBe(21);
   });
 
   it('can decrease the temp', function() {
     thermostat.down(3);
-    expect(thermostat.temp).toBe(17);
+    expect(thermostat._temp).toBe(17);
   });
 
   it('cannot decrease temp below 10', function() {
     expect(function(){ thermostat.down(11); }).toThrowError('Minimum temperature is 10!');
   });
+
+  it('cannot increase temp above 25', function() {
+    expect(function(){ thermostat.up(6); }).toThrowError('Maximum temperature is 25!');
+  });
+
+  it('has a power saving mode on by default', function() {
+    expect(thermostat._isPowerSaving).toBe(true);
+  });
+
+  it('can toggle between Power saving on and off', function() {
+    thermostat.switchPowerSaving();
+    expect(thermostat._isPowerSaving).toBe(false);
+  });
+
+  it('when power saving is off max temp is 32', function() {
+    thermostat.switchPowerSaving();
+    thermostat.up(12)
+    expect(thermostat._temp).toBe(32)
+  });
+
+  
 
 });
--- src/thermostat.js
+++ src/thermostat.js
@@ -1,17 +1,31 @@
 
 var Thermostat = function(){
-  this.temp = 20;
+  this._temp = 20;
+  this._isPowerSaving = true;
+  this._maxTemp = 25;
 };
 
 Thermostat.prototype.up = function(num){
-  this.temp += num;
+  if (this._temp+num > this._maxTemp) {
+    throw new Error("Maximum temperature is 25!")
+  } else {
+  this._temp += num;
+  }
 };
 
 Thermostat.prototype.down = function(num){
   //(temp-num > 10) ? (this.temp -= num); : throw new Error("Minimum temperature is 10!");
-  if (this.temp-num <10) {
+  if (this._temp-num <10) {
     throw new Error("Minimum temperature is 10!");
   } else {
-    this.temp -= num;
+    this._temp -= num;
   };
 };
+
+Thermostat.prototype.switchPowerSaving = function(){
+  var change = this._isPowerSaving;
+  change = !change;
+  return this._isPowerSaving = change;
+};
+
+Thermostat.prototype.changeMaxTemp = function(){};
