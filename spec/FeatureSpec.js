3e8500b493c2bab810170f2817aacfa9d055d62c
Cong Cong Bo <cong@hippocampus.ai>
2017-08-02 12:23:45 +0100

    setup working

---
+++ spec/FeatureSpec.js
@@ -0,0 +1,12 @@
+'use strict';
+
+describe('Thermostat', function() {
+  var thermostat;
+
+  beforeEach(function() {
+    thermostat = new Thermostat;
+  });
+
+  it('starts at 20', function() {
+  });
+});
